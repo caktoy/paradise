e9bcf21d66748ebc968538f123d611aa604694f0
yaqien378 <yaqien378@gmail.com>
2016-08-17 10:44:40 +0700

    JABREK SISTEM

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost:81/paradise/';
-$config['base_url'] = 'http://localhost:8080/paradise/';
+// $config['base_url'] = 'http://localhost:8080/paradise/';
+$config['base_url'] = 'http://localhost/paradise/';
 
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +37,7 @@ $config['base_url'] = 'http://localhost:8080/paradise/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
